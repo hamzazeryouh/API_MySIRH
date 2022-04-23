31a48b286b5ba409bcccb048737c63aa6d424837
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2022-04-23 21:50:30 +0200

    allow all route

--- Program.cs
+++ Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
         builder =>
         {
-            builder.WithOrigins("http://localhost:4200")
+            builder.WithOrigins("*")
             .AllowAnyMethod()
             .AllowAnyHeader()
             ;
